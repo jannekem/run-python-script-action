f2352cabc7740d13371a3eef5099973c314aa037
Janne Kemppainen <kemppainen.janne@gmail.com>
2021-01-28 20:53:58 +0200

    feat: add error handling

--- index.js
+++ index.js
@@ -4,11 +4,37 @@ const fs = require('fs');
 const tmp = require('tmp');
 
 async function run() {
+    const script = core.getInput("script");
+    const failOnError = core.getInput("fail-on-error") === "true";
+
+    let stdout = "";
+    let stderr = "";
+    let errorStatus = "false";
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            stdout += data.toString();
+        },
+        stderr: (data) => {
+            stderr += data.toString();
+        }
+    };
+
     tmp.setGracefulCleanup();
-    const script = core.getInput('script');
     const filename = tmp.tmpNameSync({postfix: '.py'});
     fs.writeFileSync(filename, script);
-    exec.exec('python', [filename])
+    try {
+        await exec.exec('python', [filename], options);
+    } catch (error) {
+        errorStatus = "true";
+        if (failOnError) {
+            core.setFailed(error);
+        }
+    } finally {
+        core.setOutput("stdout", stdout);
+        core.setOutput("stderr", stderr);
+        core.setOutput("error", errorStatus);
+    }
 }
 
 run();
