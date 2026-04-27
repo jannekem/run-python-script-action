b6754b2e7919df2750354c4bc898e1f050844b69
Janne Kemppainen <kemppainen.janne@gmail.com>
2026-04-27 21:18:52 +0300

    feat: upgrade to node24 and update dependencies

--- index.mjs
+++ index.mjs
@@ -1,14 +1,17 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const fs = require('fs');
-const tmp = require('tmp');
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import fs from 'node:fs';
+import {dirname, join} from 'node:path';
+import {fileURLToPath} from 'node:url';
+import tmp from 'tmp';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 async function run() {
     const script = core.getInput("script");
     const util = core.getInput("util") === "true";
     const failOnError = core.getInput("fail-on-error") === "true";
-    const utilityFunctions = fs.readFileSync(`${__dirname}/util.py`);
+    const utilityFunctions = fs.readFileSync(join(__dirname, "util.py"), "utf8");
 
     let stdout = "";
     let stderr = "";
@@ -24,10 +27,10 @@ async function run() {
     };
 
     tmp.setGracefulCleanup();
-    const filename = tmp.tmpNameSync({postfix: '.py'});
+    const filename = tmp.tmpNameSync({postfix: ".py"});
     fs.writeFileSync(filename, util ? utilityFunctions + script : script);
     try {
-        await exec.exec('python', [filename], options);
+        await exec.exec("python", [filename], options);
     } catch (error) {
         errorStatus = "true";
         if (failOnError) {
